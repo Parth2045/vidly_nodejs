dc4c69b7eff37ccfae27ded817401532befdcff5
Parth Patel <you@example.com>
2024-09-20 18:17:51 +0530

    fix: added cors support

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@ import express from 'express';
 import cors from 'cors'; // ADDED DUE TO CORS ISSUE
 const app = express();
 
-app.use(cors()); // ADDED DUE TO CORS ISSUE
-app.options('*', cors());
+// ADDED DUE TO CORS ISSUE
+app.use(cors());
+// app.options('*', cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+// ADDED DUE TO CORS ISSUE
 
 import startLogging from './startup/logging';
 startLogging();
